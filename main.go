41de1a4572cd51a89d02b517b990a5c35fff2e04
postog78 <postog@mail.ru>
2020-04-26 22:05:12 +0300

    Уже работает

--- main.go
+++ main.go
@@ -1,87 +1,91 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
-	"net/http"
-	"os"
 
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
+
 	"google.golang.org/api/sheets/v4"
 )
 
-// Retrieve a token, saves the token, then returns the generated client.
-func getClient(config *oauth2.Config) *http.Client {
-	// The file token.json stores the user's access and refresh tokens, and is
-	// created automatically when the authorization flow completes for the first
-	// time.
-	tokFile := "token.json"
-	tok, err := tokenFromFile(tokFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(tokFile, tok)
-	}
-	return config.Client(context.Background(), tok)
-}
+// // Retrieve a token, saves the token, then returns the generated client.
+// func getClient(config *oauth2.Config) *http.Client {
+// 	// The file token.json stores the user's access and refresh tokens, and is
+// 	// created automatically when the authorization flow completes for the first
+// 	// time.
+// 	tokFile := "token.json"
+// 	tok, err := tokenFromFile(tokFile)
+// 	if err != nil {
+// 		tok = getTokenFromWeb(config)
+// 		saveToken(tokFile, tok)
+// 	}
+// 	return config.Client(context.Background(), tok)
+// }
 
 // Request a token from the web, then returns the retrieved token.
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
+// func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
+// 	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
+// 	fmt.Printf("Go to the following link in your browser then type the "+
+// 		"authorization code: \n%v\n", authURL)
 
-	var authCode string
-	if _, err := fmt.Scan(&authCode); err != nil {
-		log.Fatalf("Unable to read authorization code: %v", err)
-	}
+// 	var authCode string
+// 	if _, err := fmt.Scan(&authCode); err != nil {
+// 		log.Fatalf("Unable to read authorization code: %v", err)
+// 	}
 
-	tok, err := config.Exchange(context.TODO(), authCode)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web: %v", err)
-	}
-	return tok
-}
+// 	tok, err := config.Exchange(context.TODO(), authCode)
+// 	if err != nil {
+// 		log.Fatalf("Unable to retrieve token from web: %v", err)
+// 	}
+// 	return tok
+// }
 
-// Retrieves a token from a local file.
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
-	if err != nil {
-		return nil, err
-	}
-	defer f.Close()
-	tok := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(tok)
-	return tok, err
-}
+// // Retrieves a token from a local file.
+// func tokenFromFile(file string) (*oauth2.Token, error) {
+// 	f, err := os.Open(file)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	defer f.Close()
+// 	tok := &oauth2.Token{}
+// 	err = json.NewDecoder(f).Decode(tok)
+// 	return tok, err
+// }
 
-// Saves a token to a file path.
-func saveToken(path string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", path)
-	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
+// // Saves a token to a file path.
+// func saveToken(path string, token *oauth2.Token) {
+// 	fmt.Printf("Saving credential file to: %s\n", path)
+// 	f, err := os.OpenFile(path, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
+// 	if err != nil {
+// 		log.Fatalf("Unable to cache oauth token: %v", err)
+// 	}
+// 	defer f.Close()
+// 	json.NewEncoder(f).Encode(token)
+// }
 
 func main() {
-	b, err := ioutil.ReadFile("credentials.json")
+	// b, err := ioutil.ReadFile("credentials.json")
+	// b, err := ioutil.ReadFile("client_secret_510713154079-5oan62fqccksj647mt12v025rdo3jle1.apps.googleusercontent.com.json")
+	b, err := ioutil.ReadFile("diversityshipments-31d3872ee94b.json")
+
 	if err != nil {
 		log.Fatalf("Unable to read client secret file: %v", err)
 	}
 
 	// If modifying these scopes, delete your previously saved token.json.
 	// config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets.readonly")
-	config, err := google.ConfigFromJSON(b, "https://www.googleapis.com/auth/spreadsheets")
+	ctx := context.Background()
+	// creds, err := google.CredentialsFromJSON(ctx, b, "https://www.googleapis.com/auth/spreadsheets")
+	creds, err := google.CredentialsFromJSON(ctx, b, "https://www.googleapis.com/auth/spreadsheets.readonly")
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		log.Fatal(err)
 	}
-	client := getClient(config)
+
+	client := oauth2.NewClient(ctx, creds.TokenSource) //credentials. getClient(config)
 
 	srv, err := sheets.New(client)
 	if err != nil {
@@ -91,7 +95,7 @@ func main() {
 	// Prints the names and majors of students in a sample spreadsheet:
 	// https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
 	spreadsheetId := "1_EokmBJWtNKYMbQZEeuTrzk7fwLy5z1H-IbzR1v2q14"
-	readRange := "Class Data!A2:E"
+	readRange := "Продажа Апрель 2020!A2:E2"
 	resp, err := srv.Spreadsheets.Values.Get(spreadsheetId, readRange).Do()
 	if err != nil {
 		log.Fatalf("Unable to retrieve data from sheet: %v", err)
@@ -107,3 +111,7 @@ func main() {
 		}
 	}
 }
+
+// Просто ключ AIzaSyBg2jgYgXgjlhqTJNK3iN3KVeNE9acf0vU
+// Идентификатор клиента 510713154079-5oan62fqccksj647mt12v025rdo3jle1.apps.googleusercontent.com
+// Секретный код клиента 7O2XOJbj8GGR3oXtVnlU5pH5
